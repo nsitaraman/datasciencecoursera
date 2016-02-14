4bc47f8b79271c3d2f4fcfdde02689eb3a89ccd5
nsitaraman <nsitaramanz@gmail.com>
2016-02-13 23:00:32 -0500

    Committed week3 assignment files

---
+++ R Programming/week3/cachematrix.R
@@ -0,0 +1,34 @@
+##These functions allow caching a matrix's inverse so the potentially time-consuming
+#calculations only need to be performed once
+
+##makeCacheMatrix creates a special matrix data structure that returns a list
+#containing getter/setter functions for the matrix and its inverse
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+
+##cacheSolve takes a cacheMatrix structure and returns its inverse.
+#the inverse is only calculated once and cached for subsequent function calls.
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
+}
